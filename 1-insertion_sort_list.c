abfdd469fa27b3a785fedce5ae81a813d5a7f30a
hedi <hedihamida98@gmail.com>
2022-08-25 10:36:50 +0100

    new

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * helper - swap
+ * @list: list
+ * @prenode: prenode
+ * @current: current
+ */
+void helper(listint_t **list, listint_t *prenode, listint_t *current)
+{
+
+	prenode->next = current->next;
+	current->prev = prenode->prev;
+	current->next = prenode;
+	prenode->prev = current;
+	print_list(*list);
+}
+/**
+ * insertion_sort_list - sort a list using insertion algo
+ * @list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = *list, *current, *prenode;
+
+	while (node != NULL)
+	{
+
+		current = node;
+		while (node->prev != NULL && node->prev->n > node->n)
+		{
+			prenode = node->prev;
+			if (current->next != NULL)
+				current->next->prev = prenode;
+			if (prenode->prev != NULL)
+				prenode->prev->next = current;
+			else
+				*list = current;
+
+			helper(list, prenode, current);
+		}
+		node = node->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,5 @@ void print_list(const listint_t *list);
 void swap(int *array, size_t size, int j);
 
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
 #endif
