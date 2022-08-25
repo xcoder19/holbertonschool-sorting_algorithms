69dbf9b7dc5228ff7e49dbbf25b35e3c688e256d
hedi <hedihamida98@gmail.com>
2022-08-25 23:12:39 +0100

    new

---
+++ 3-quick_sort.c
@@ -0,0 +1,58 @@
+
+#include "sort.h"
+void helper(int *array, int idx0, int idxn, size_t size);
+/**
+ * swap - swap
+ * @array: array
+ * @i: index
+ * @j: index
+ * @size: size
+ */
+void swap(int *array, int i, int j, size_t size)
+{
+	int tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+	print_array(array, size);
+}
+/**
+ * quick_sort - quick sort
+ * @array: array
+ * @size: size
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	helper(array, 0, size - 1, size);
+}
+/**
+ * helper - quick sort with Lomuto partition scheme
+ * @idx0: index0
+ * @idxn: last index
+ * @size: size
+ */
+void helper(int *array, int idx0, int idxn, size_t size)
+{
+	int i, j, pivot;
+
+	if (idxn > idx0)
+	{
+		pivot = idx0;
+		i = idx0;
+		j = idxn;
+
+		while (j > i)
+		{
+			if (array[i] <= array[pivot] && i < idxn)
+				i++;
+			while (array[j] > array[pivot])
+				j--;
+
+			if (j > i)
+				swap(array, i, j, size);
+		}
+		swap(array, pivot, j, size);
+		helper(array, idx0, j - 1, size);
+		helper(array, j + 1, idxn, size);
+	}
+}
